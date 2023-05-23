e7fe25d2231894c8b64bdd5647154eb2ca9c82a5
JehanKandy <jehankandy@gmail.com>
2023-05-23 06:51:16 +0530

    Update client.php
            if(!Auth::check()){
                return redirect('/login');
            }
    
            $user=Auth::user();
            if($user->role==4){
                return $next($request);
            }
            if($user->role==1){
                return redirect('/superadmin');
            }
            if($user->role==3){
                return redirect('/staff');
            }
            if($user->role==2){
                return redirect('/admin');
            }
    
    Update user redireactions

--- app/Http/Middleware/client.php
+++ app/Http/Middleware/client.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class client
@@ -15,6 +16,22 @@ class client
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if(!Auth::check()){
+            return redirect('/login');
+        }
+
+        $user=Auth::user();
+        if($user->role==4){
+            return $next($request);
+        }
+        if($user->role==1){
+            return redirect('/superadmin');
+        }
+        if($user->role==3){
+            return redirect('/staff');
+        }
+        if($user->role==2){
+            return redirect('/admin');
+        }
     }
 }
